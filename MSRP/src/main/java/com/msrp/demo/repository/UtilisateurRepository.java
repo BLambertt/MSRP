f55d8cd7efa4b952504ba69ce6af137f9f87adbd
Laurent Le Foulgoc <laurent.le-foulgoc@epitech.eu>
2024-01-31 11:31:58 +0100

    Utilisateurs - le controlleur est créé mais semble non fonctionnel. UtilisateurRepository a obtenu un decoreteur qui permet d'appeler les utilisateurs

--- MSRP/src/main/java/com/msrp/demo/repository/UtilisateurRepository.java
+++ MSRP/src/main/java/com/msrp/demo/repository/UtilisateurRepository.java
@@ -3,7 +3,9 @@ package com.msrp.demo.repository;
 
 import com.msrp.demo.model.Utilisateur;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="users", collectionResourceRel = "tab")
 public interface UtilisateurRepository extends JpaRepository<Utilisateur, Integer> {
 
 }
